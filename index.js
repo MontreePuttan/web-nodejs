ede45d8b6542ebc2d8c70e89b3b0fee844657ae0
Montree <montree@mohara.co>
2017-07-14 17:54:39 +0700

    Insert Data

--- index.js
+++ index.js
@@ -13,4 +13,9 @@ var con = mysql.createConnection({
 con.connect(function(err){
 	if(err) throw err;
 	console.log("Connected");
+	sql = "INSERT INTO `product` (`product_id`, `product_name`, `brand_id`, `product_price`, `product_image`) VALUES ('2', '2222', '2', '222', '22222');";
+	con.query(sql,function(err,result){
+		if(err) throw err;
+		console.log("Insert Complete");
+	});
 });
