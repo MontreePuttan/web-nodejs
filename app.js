a3233c75d138237c95141dfdf5b4d20035f96095
Montree <montree@mohara.co>
2017-07-17 14:32:19 +0700

    GET Method

--- app.js
+++ app.js
@@ -44,5 +44,19 @@ app.get('/database',function(req,res){
 	res.render('database');
 });
 
+// method get
+app.get('/showForm',function(req,res){
+	res.sendFile(__dirname + "/" + "showForm.html");
+});
+
+app.get('/showData',function(req,res){
+	data = {
+		fname:req.query.fname,
+		lname:req.query.lname
+	};
+	console.log(data);
+	res.end(JSON.stringify(data));
+});
+
 
 app.listen(8081);
