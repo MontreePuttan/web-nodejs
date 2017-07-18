431e3dc3de12a6fae14d1994282050a4e867c884
Montree <montree@mohara.co>
2017-07-18 17:06:20 +0700

    Sending Emails using Nodemailer

--- app.js
+++ app.js
@@ -1,97 +1,300 @@
 var express = require('express');
-var cookie = require('cookie-parser');
-var body = require('body-parser');
+var bodyParser = require('body-parser');
 var app = express();
+var db = require('./db');
+var cookie = require('cookie-parser');
+var session = require('express-session');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-app.use(body());
+var nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    port: 25,
+    auth: {
+        user: 'scoopy825@gmail.com',
+        pass: '027571563'
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+let HelperOption = {
+    from: '"Natty" <scoopy825@gmail.com>',
+    to: 'montree@mohara.co',
+    subject: 'Hello World',
+    text: 'This is email'
+};
+transporter.sendMail(HelperOption,(error,info)=>{
+    if(error){
+        return console.log(error);
+    }
+    console.log("The message");
+    console.log(info);
+});
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'))
+
 app.use(cookie());
-app.set('view engine','ejs');
+app.use(session({ secret: 'keyboard_cat',resave: false,saveUninitialized: true}));
 
 
 
-app.get('/',function(req,res){
-	res.send('<h1>HelloWorld</h1>');
-});
 
-app.get('/profile',function(req,res){
-	res.send('<h1>Profile</h1>');
-});
 
-// route param name
-app.get('/profile/:name',function(req,res){
-	res.send('<h1>Profile '+ req.params.name +'</h1>');
-});
 
-// route param id
-app.get('/profile/:id',function(req,res){
-	res.send('<h1>Profile '+ req.params.id +'</h1>');
-});
+app.get('/createSession2', function (req, res) {
+    //req.session.name = req.params.user;
 
-// Middleware
-app.use('/user/profile/:name',function(req,res,next){
-	console.log('Resquest : '+ req.params.name +' = '+ new Date(),req.method,req.url);
-});
-
-//Ejs Template Engine
-app.get('/template',function(req,res){
-	var data = {name:"Natty",age:26,job:"Programmer"};
-	res.render('profile',{user:data});
+    //res.send(req.session.name);
+    req.session.name = 'Napoleon';
+    var name = req.session.name;
+    res.send(name);
+    //res.end("Create YoYo");
 });
 
 
-// show page
-app.get('/home',function(req,res){
-	res.render('home');
+
+app.get('/createSession', function(req, res) {
+    req.session.from_session = 30000000;
+    res.render('showSession', {session: req.session.from_session});
+    //res.redirect('/session');
+
 });
 
-app.get('/contact',function(req,res){
-	res.render('contact');
+app.get('/delSession', function(req, res) {
+    req.session.destroy(function() {
+    res.send('Session deleted');
+    });
 });
 
-app.get('/database',function(req,res){
-	res.render('database');
-});
-
-// method get
-app.get('/showFormGet',function(req,res){
-	res.sendFile(__dirname + "/" + "showFormGet.html");
-});
-
-app.get('/showDataGet',function(req,res){
-	data = {
-		fname:req.query.fname,
-		lname:req.query.lname
-	};
-	console.log(data);
-	res.end(JSON.stringify(data));
+app.get('/showSession', function(req, res) {
+    res.render('showSession', {session: req.session.from_session});
 });
 
 
-// method post
-app.get('/showFormPost',function(req,res){
-	res.sendFile(__dirname + "/" + "showFormPost.html");
+
+
+
+
+app.get('/createCookie', function (req, res) {
+    res.cookie('myCookie','Cookie_Montree');
+    res.end("Create Cookie");
 });
 
-app.post('/showDataPost',function(req,res){
-	data = {
-		fname:req.body.fname,
-		lname:req.body.lname
-	};
-	console.log(data);
-	res.end(JSON.stringify(data));
-});
-
-
-// cookie
-app.get('/createCookie',function(req,res){
-	res.cookie('myCookie','montree');
-	res.end('Create Cookie');
-});
-
-app.get('/deleteCookie',function(req,res){
-	res.clearCookie('myCookie');
-	res.end('Delete Cookie');
+app.get('/deleteCookie', function (req, res) {
+    res.clearCookie('myCookie','Cookie_Montree');
+    res.end("Delete Cookie");
 });
 
 
-app.listen(8081);
+app.get('/', function (req, res) {
+    res.send('Hello');
+});
+
+app.get('/profile/:id', function (req, res) {
+    //console.log(req.url);
+    res.send('Hi ID : ' + req.params.id);
+});
+
+app.get('/contact', function (req, res) {
+    res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/about/:name', function (req, res) {
+    //console.log(req.url);
+    var datas = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
+    res.render('about', {person: req.params.name, data: datas});
+});
+
+/*
+app.get('/location', function (req, res) {
+    //console.log(req.url);
+    res.render('locations');
+});
+*/
+
+// call ฟังค์ชัน getLocation เมื่อ client เข้าถึงหน้าเว็บ /about
+app.get('/location', getLocation);
+
+function getLocation(req, res) {
+    res.render('locations');
+}
+
+
+//method get
+app.get('/queryStrings', function (req, res) {
+    console.log(req.query);
+    res.render('queryStrings', {qs: req.query});
+});
+
+//method post
+app.post('/queryStrings', urlencodedParser, function (req, res) {
+    console.log(req.body);
+    res.render('queryStrings-success', {data: req.body});
+});
+
+app.get('/show', function (req, res) {
+
+    db.query('SELECT * FROM users', function (err, rows, fields)
+    {
+        if (err)
+            throw err;
+
+        //console.log(rows[0]); 
+        res.render('showData', {data: rows});
+        
+    });
+});
+
+app.get('/insert', function (req, res) {
+
+    var post = {firstname: 'nat', lastname: 'tan', email: 'ttt@g.com'};
+    var query = db.query('INSERT INTO users SET ?', post, function (err, result) {
+        // Neat!
+    });
+    console.log(query.sql);
+    res.send('Pass');
+});
+
+app.get('/formupdate', function (req, res) {
+
+    res.render('formUpdate');
+});
+
+app.get('/forminsert', function (req, res) {
+
+    res.render('forminsert');
+});
+
+app.post('/forminsert', urlencodedParser, function (req, res) {
+    
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var email = req.body.email;
+    
+    var post = {firstname: firstname, lastname: lastname, email: email};
+    var query = db.query('INSERT INTO users SET ?', post, function (err, result) {
+        if (query) {
+            res.redirect('/show');
+        } else {
+            console.log('No');
+        }
+    });
+
+    //res.send('Hi ID : ' + req.body.firstname);
+    //console.log(firstname);
+});
+
+app.get('/getformupdate/:id', function (req, res) {
+
+    var id = req.params.id;
+    //console.log(id); 
+
+
+    db.query('SELECT * FROM users where id = ?', id, function (err, rows, fields)
+    {
+        if (err)
+            throw err;
+
+        //console.log(rows); 
+        res.render('formupdate', {data: rows});
+    });
+
+
+});
+
+app.get('/deleteData/:id', function (req, res) {
+    //res.send('Hi ID : ' + req.params.id);
+   
+    
+    var id = req.params.id;
+    //console.log(id); 
+
+
+
+    var query = db.query('DELETE FROM users where id = ?', id, function (err, rows, fields) {
+        if (query) {
+            res.redirect('/show');
+        } else {
+            console.log('No');
+        }
+    });
+
+    
+
+});
+
+app.post('/getformupdate', urlencodedParser, function (req, res) {
+
+    //console.log(req.body);
+
+    var id = req.body.id;
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var email = req.body.email;
+
+    var query = db.query('update users SET firstname = ? , lastname = ? , email = ? where id = ?', [firstname, lastname, email, id], function (err, result) {
+
+        if (query) {
+            res.redirect('/show');
+            console.log('Yes');
+            var b = new Buffer('123');
+            var s = b.toString('base64');
+            var m = b.toString('utf8');
+            console.log(s);
+            console.log(m);
+        } else {
+            console.log('No');
+        }
+
+    });
+
+
+    //console.log(query.sql);
+});
+
+
+app.get('/update', function (req, res) {
+
+    //console.log(query.sql);
+    res.send('Pass');
+    /*
+     var text = 'nat222';
+     var ids = 7;
+     var query = db.query('update users SET firstname = ? where id = ?', [text,ids], function(err, result) {
+     
+     });
+     console.log(query.sql);
+     res.send('Pass');
+     */
+});
+
+//Template-freelancer
+app.get('/template', function (req, res) {
+
+    res.render('template-freelancer/content-full-width');
+});
+
+
+app.get('/template', function (req, res) {
+
+    res.send("Send mail");
+});
+
+
+//404 page
+app.get('*', function (req, res) {
+    res.render('404');
+});
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server start port 3000");
+});
---
+++ connect.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');  
+var connection = mysql.createConnection({  
+  host     : 'localhost',  
+  user     : 'root',  
+  password : 'root',  
+  database : 'web-nodejs'  
+});  
+//connection.connect();  
+
+connection.connect(function(err){
+  if(err){
+    console.log('Database connection error');
+  }else{
+    console.log('Database connection successful');
+  }
+});
+
+module.exports = connection;
